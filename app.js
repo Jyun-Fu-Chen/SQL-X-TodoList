a89c4fae58e574c0b48a625de49973ff9af5568b
Jyun-Fu-Chen <than3698741@gmail.com>
2022-04-13 22:25:55 +0800

    add login & register page

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
-const res = require('express/lib/response')
 const app = express()
 
 const PORT = 3000
@@ -16,6 +15,26 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(PORT, () => {
-console.log(`localhost${PORT} is running!!`)
+  console.log(`localhost${PORT} is running!!`)
 })
